30130e7721cff5cddd5e6cebe000fb5bfc96f3c6
JustPav <aksenovpaveldmitrievich@gmail.com>
2026-05-15 17:12:18 +0400

    New: New modules added in accordance with library changes.

--- lib/build.zig
+++ lib/build.zig
@@ -8,13 +8,6 @@ pub fn build(b: *std.Build) void {
     const strip = b.option(bool, "strip", "Strip debug symbols") orelse false;
     const stack_check = b.option(bool, "stack-check", "Check for stack overflows") orelse false;
 
-    // ── Constants ──────────────────────────────────────────────────────────────
-    const upac_constants = b.createModule(.{
-        .root_source_file = b.path("src/constants.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     // ── C libs ─────────────────────────────────────────────────────────────────
     const translated_libs = b.addTranslateC(.{
         .root_source_file = b.path("src/imports.h"),
@@ -31,82 +24,105 @@ pub fn build(b: *std.Build) void {
 
     const translated_libs_module = translated_libs.createModule();
 
-    // ── Types and ffi ─────────────────────────────────────────────────────────────────
+    // ── Types ──────────────────────────────────────────────────────────────
+    const upac_types = b.createModule(.{
+        .root_source_file = b.path("src/types/types.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // ── FFI ─────────────────────────────────────────────────────────────────
     const upac_ffi = b.createModule(.{
-        .root_source_file = b.path("src/ffi/ctypes.zig"),
+        .root_source_file = b.path("src/ffi.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     upac_ffi.addImport("clibs", translated_libs_module);
+    upac_ffi.addImport("upac-types", upac_types);
 
-    // ── Database ──────────────────────────────────────────────────────────────
-    const upac_data = b.createModule(.{
-        .root_source_file = b.path("src/data/data.zig"),
+    // ── Index ──────────────────────────────────────────────────────────────
+    const upac_index = b.createModule(.{
+        .root_source_file = b.path("src/index.zig"),
         .target = target,
         .optimize = optimize,
     });
-    upac_data.addImport("upac-ffi", upac_ffi);
-    upac_data.addImport("upac-constants", upac_constants);
+    upac_index.addImport("upac-types", upac_types);
+
+    // ── Database ──────────────────────────────────────────────────────────────
+    const upac_database = b.createModule(.{
+        .root_source_file = b.path("src/database.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    upac_database.addImport("upac-types", upac_types);
 
     // ── Installer ─────────────────────────────────────────────────────────────
     const upac_installer = b.createModule(.{
-        .root_source_file = b.path("src/commands/installer/installer.zig"),
+        .root_source_file = b.path("src/installer/installer.zig"),
         .target = target,
         .optimize = optimize,
     });
+    upac_installer.addImport("upac-types", upac_types);
     upac_installer.addImport("upac-ffi", upac_ffi);
-    upac_installer.addImport("upac-data", upac_data);
-    upac_installer.addImport("upac-constants", upac_constants);
+
+    upac_installer.addImport("upac-data", upac_database);
+    upac_installer.addImport("upac-index", upac_index);
 
     // ── Uninstaller ───────────────────────────────────────────────────────────
     const upac_uninstaller = b.createModule(.{
-        .root_source_file = b.path("src/commands/uninstaller/uninstaller.zig"),
+        .root_source_file = b.path("src/uninstaller/uninstaller.zig"),
         .target = target,
         .optimize = optimize,
     });
+    upac_uninstaller.addImport("upac-types", upac_types);
     upac_uninstaller.addImport("upac-ffi", upac_ffi);
-    upac_uninstaller.addImport("upac-data", upac_data);
-    upac_uninstaller.addImport("upac-constants", upac_constants);
+
+    upac_uninstaller.addImport("upac-index", upac_index);
+    upac_uninstaller.addImport("upac-database", upac_database);
 
     // ── Rollback ────────────────────────────────────────────────────────────────
     const upac_rollback = b.createModule(.{
-        .root_source_file = b.path("src/commands/rollback/rollback.zig"),
+        .root_source_file = b.path("src/rollback/rollback.zig"),
         .target = target,
         .optimize = optimize,
     });
+    upac_rollback.addImport("upac-types", upac_types);
     upac_rollback.addImport("upac-ffi", upac_ffi);
-    upac_rollback.addImport("upac-constants", upac_constants);
 
     // ── Diff ────────────────────────────────────────────────────────────────
     const upac_diff = b.createModule(.{
-        .root_source_file = b.path("src/commands/diff/diff.zig"),
+        .root_source_file = b.path("src/diff/diff.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    upac_diff.addImport("upac-types", upac_types);
     upac_diff.addImport("upac-ffi", upac_ffi);
-    upac_diff.addImport("upac-data", upac_data);
-    upac_diff.addImport("upac-constants", upac_constants);
+
+    upac_diff.addImport("upac-database", upac_database);
 
     // ── List ────────────────────────────────────────────────────────────────
     const upac_list = b.createModule(.{
-        .root_source_file = b.path("src/commands/list/list.zig"),
+        .root_source_file = b.path("src/list/list.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    upac_list.addImport("upac-types", upac_types);
     upac_list.addImport("upac-ffi", upac_ffi);
-    upac_list.addImport("upac-data", upac_data);
-    upac_list.addImport("upac-constants", upac_constants);
+
+    upac_list.addImport("upac-database", upac_database);
 
     // ── Init ──────────────────────────────────────────────────────────────────
-    const upac_init = b.createModule(.{
-        .root_source_file = b.path("src/commands/init.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    upac_init.addImport("upac-ffi", upac_ffi);
-    upac_init.addImport("upac-data", upac_data);
-    upac_init.addImport("upac-constants", upac_constants);
+    // const upac_init = b.createModule(.{
+    //     .root_source_file = b.path("src/init.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // upac_init.addImport("upac-ffi", upac_ffi);
+    // upac_init.addImport("upac-data", upac_data);
+    // upac_init.addImport("upac-constants", upac_constants);
 
     // ── Root ──────────────────────────────────────────────────────────────────
     const upac_lib_root = b.createModule(.{
@@ -128,17 +144,19 @@ pub fn build(b: *std.Build) void {
     shared_lib.root_module.link_libc = true;
     shared_lib.root_module.addImport("clibs", translated_libs_module);
 
+    shared_lib.root_module.addImport("upac-types", upac_types);
     shared_lib.root_module.addImport("upac-ffi", upac_ffi);
-    shared_lib.root_module.addImport("upac-data", upac_data);
-    shared_lib.root_module.addImport("upac-constants", upac_constants);
+
+    shared_lib.root_module.addImport("upac-database", upac_database);
+    shared_lib.root_module.addImport("upac-index", upac_index);
 
     shared_lib.root_module.addImport("upac-installer", upac_installer);
     shared_lib.root_module.addImport("upac-uninstaller", upac_uninstaller);
-    shared_lib.root_module.addImport("upac-rollback", upac_rollback);
+    // shared_lib.root_module.addImport("upac-rollback", upac_rollback);
 
     shared_lib.root_module.addImport("upac-diff", upac_diff);
     shared_lib.root_module.addImport("upac-list", upac_list);
-    shared_lib.root_module.addImport("upac-init", upac_init);
+    // shared_lib.root_module.addImport("upac-init", upac_init);
 
     b.installArtifact(shared_lib);
 
